de5e1097fe83f803f5c4e4255c1f70b92f8fbabf
James Appello <jamesappello@gmail.com>
2019-09-18 21:42:02 -0400

    update1

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,4 @@
 // GET route to '/survey' displays survey.html page
 // DEFAULT: 'catch-all' route that leads to home.html
+const fs = require('fs');
+//fs.readFile("server.js");
--- server.js
+++ server.js
@@ -2,3 +2,14 @@
 const express = require('express');
 // require path
 const path = require('path');
+const app = express();
+
+
+app.get("/", (request, response) => {
+    resizeBy.sendFile(path.join(__dirname,"public", "home.html"));
+})
+
+
+const PORT = process.env.PORT || 6969;
+
+app.listen(PORT, () => console.log(`SEVER BEGAN LISTENING ON PORT: ${PORT}`));
